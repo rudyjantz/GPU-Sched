390c6e98b5290229c3dbcd9f941d0c2c710bf9c6
rudy jantz <porter@gatech.edu>
2020-08-05 10:33:43 -0400

    improve parsing and automation for results

--- src/runtime/driver/parse_workloader_results.py
+++ src/runtime/driver/parse_workloader_results.py
@@ -1,41 +1,127 @@
 #!/usr/bin/env python3
-
 import sys
-
-#
-# Examples of what we're looking for:
-#
-# bmark times:
-#   Worker 0: TOTAL_BENCHMARK_TIME 0 2.99345064163208
-#   Worker 1: TOTAL_BENCHMARK_TIME 2 1.2312407493591309
-# total time:
-#   Worker 0: TOTAL_EXPERIMENT_TIME 4.400961637496948
-#
+import statistics
+from scipy.stats.mstats import gmean
 
 
 
-if len(sys.argv) == 2:
-    filename = sys.argv[1]
-else:
-    filename = 'outch'
 
-bmark_times = []
-total_time = 0
+BASE_PATH = '/home/rudy/wo/gpu/bes-gpu/foo/scripts/cc/results-2020.08.01-7.30pm'
+SCHED_LOG_SUF  = 'sched-log'
+SCHED_STAT_SUF = 'sched-stats'
+WRKLDR_LOG_SUF = 'workloader-log'
 
 
-with open(filename) as f:
-    for line in f:
-        if 'TOTAL_BENCHMARK_TIME' in line:
-            line = line.strip().split()
-            bmark_times.append((int(line[3]), line[4]))
-        elif 'TOTAL_EXPERIMENT_TIME' in line:
-            line = line.strip().split()
-            total_time = line[3]
+DEBUG = False
 
-throughput = len(bmark_times) / float(total_time)
 
-sorted_bmark_times = sorted(bmark_times)
-for t in sorted_bmark_times:
-    print('{} {}'.format(t[0], t[1]))
-print('total {}'.format(total_time))
-print('throughput {}'.format(throughput))
+# TODO: move to input files and support command-line args
+workloads = [
+    'k80_small_16jobs_0',
+    'k80_small_16jobs_1',
+    'k80_medium_16jobs_0',
+    'k80_medium_16jobs_1',
+    'k80_large_16jobs_0',
+    'k80_large_16jobs_1',
+]
+
+
+
+
+def print_debug(s):
+    if DEBUG:
+        print(s)
+
+
+def parse_workloader_log(filename):
+    # Examples of what we're looking for:
+    #
+    # bmark times:
+    #   Worker 0: TOTAL_BENCHMARK_TIME 0 2.99345064163208
+    #   Worker 1: TOTAL_BENCHMARK_TIME 2 1.2312407493591309
+    # total time:
+    #   Worker 0: TOTAL_EXPERIMENT_TIME 4.400961637496948
+    bmark_times = []
+    total_time  = 0
+    throughput = 0
+    print_debug(filename)
+    with open(filename) as f:
+        for line in f:
+            if 'TOTAL_BENCHMARK_TIME' in line:
+                line = line.strip().split()
+                bmark_times.append( (int(line[3]), float(line[4])) )
+            elif 'TOTAL_EXPERIMENT_TIME' in line:
+                line = line.strip().split()
+                total_time = float(line[3])
+
+    throughput = len(bmark_times) / float(total_time)
+
+    sorted_bmark_times = sorted(bmark_times)
+    for t in sorted_bmark_times:
+        print_debug('{} {}'.format(t[0], t[1]))
+    print_debug('total {}'.format(total_time))
+    print_debug('throughput {}'.format(throughput))
+    return sorted_bmark_times, total_time, throughput
+
+
+def report_total_time_and_throughput(workload):
+    print('{}'.format(workload))
+    print('scheduler total_time throughput')
+    print('sa  {} {}'.format(sa_total_time, sa_throughput))
+    print('cg  {} {}'.format(cg_total_time, cg_throughput))
+    print('mgb {} {}'.format(mgb_total_time, mgb_throughput))
+    print()
+
+
+def report_avg_speedup_throughput_improvement_and_job_slowdown():
+    cg_speedups  = [ t[0] / t[1] for t in zip(sa_total_times, cg_total_times) ]
+    mgb_speedups = [ t[0] / t[1] for t in zip(sa_total_times, mgb_total_times) ]
+    print('avg_cg_speedup:                 {}'.format(statistics.mean(cg_speedups)))
+    print('avg_mgb_speedup:                {}'.format(statistics.mean(mgb_speedups)))
+
+    cg_throughput_improvements  = [ t[1] / t[0] for t in zip(sa_throughputs, cg_throughputs) ]
+    mgb_throughput_improvements = [ t[1] / t[0] for t in zip(sa_throughputs, mgb_throughputs) ]
+    print('avg_cg_throughput_improvement:  {}'.format(statistics.mean(cg_throughput_improvements)))
+    print('avg_mgb_throughput_improvement: {}'.format(statistics.mean(mgb_throughput_improvements)))
+
+    cg_job_slowdowns  = [ t[0][1] / t[1][1] for t in zip(cg_job_times, sa_job_times) ]
+    mgb_job_slowdowns = [ t[0][1] / t[1][1] for t in zip(mgb_job_times, sa_job_times) ]
+    print('mean_cg_job_slowdown:     {}'.format(statistics.mean(cg_job_slowdowns)))
+    print('geomean_cg_job_slowdown:  {}'.format(gmean(cg_job_slowdowns)))
+    print('mean_mgb_job_slowdown:    {}'.format(statistics.mean(mgb_job_slowdowns)))
+    print('geomean_mgb_job_slowdown: {}'.format(gmean(mgb_job_slowdowns)))
+
+
+
+
+sa_total_times  = []
+cg_total_times  = []
+mgb_total_times = []
+sa_throughputs  = []
+cg_throughputs  = []
+mgb_throughputs = []
+sa_job_times    = []
+cg_job_times    = []
+mgb_job_times   = []
+for workload in workloads:
+    sa_filename  = '{}/{}.{}.{}'.format(BASE_PATH, workload, 'single-assignment', WRKLDR_LOG_SUF)
+    cg_filename  = '{}/{}.{}.{}'.format(BASE_PATH, workload, 'cg', WRKLDR_LOG_SUF)
+    mgb_filename = '{}/{}.{}.{}'.format(BASE_PATH, workload, 'mgb', WRKLDR_LOG_SUF)
+
+    sa_times, sa_total_time, sa_throughput    = parse_workloader_log(sa_filename)
+    cg_times, cg_total_time, cg_throughput    = parse_workloader_log(cg_filename)
+    mgb_times, mgb_total_time, mgb_throughput = parse_workloader_log(mgb_filename)
+
+    sa_job_times.extend(sa_times)
+    cg_job_times.extend(cg_times)
+    mgb_job_times.extend(mgb_times)
+    sa_total_times.append(sa_total_time)
+    cg_total_times.append(cg_total_time)
+    mgb_total_times.append(mgb_total_time)
+    sa_throughputs.append(sa_throughput)
+    cg_throughputs.append(cg_throughput)
+    mgb_throughputs.append(mgb_throughput)
+
+    report_total_time_and_throughput(workload)
+
+report_avg_speedup_throughput_improvement_and_job_slowdown()
