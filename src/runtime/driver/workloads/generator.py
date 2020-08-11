c52dd40ee055f8e9165efe6e270374ab8e1c6c78
r jantz <porter@gatech.edu>
2020-08-11 20:48:45 +0000

    updates to generator to creates mixes with the ratios we provide

--- src/runtime/driver/workloads/generator.py
+++ src/runtime/driver/workloads/generator.py
@@ -59,7 +59,6 @@ all_jobs = [
     (  374640000, 'myocyte/myocyte.out 1000 1000 1'),
     (  400400000, 'pathfinder/pathfinder 100000 1000 20'),
     (  402653184, 'srad/srad_v2/srad 4096 4096 0 127 0 127 0.5 2'),
-    (  419466152, 'particlefilter/particlefilter_float -x 2048 -y 2048 -z 100 -np 100'),
     (  520093696, 'dwt2d/dwt2d {}/dwt2d/rgb.bmp -d 4096x4096 -f -5 -l 3'.format(RODINIA_DATA_PATH)),
     (  537001992, 'nw/needle 8192 10'),
     (  589299988, 'backprop/backprop 4194304'),
@@ -71,8 +70,8 @@ all_jobs = [
     ( 2080374784, 'dwt2d/dwt2d {}/dwt2d/rgb.bmp -d 8192x8192 -f -5 -l 3'.format(RODINIA_DATA_PATH)),
     ( 2147745800, 'nw/needle 16384 10'),
     ( 2350907668, 'backprop/backprop 16777216'),
-    #( 3686640000, 'myocyte/myocyte.out 10000 1000 1'),
     ( 3872176000, 'srad/srad_v1/srad 100 0.5 11000 11000'),
+    # ~ 4 GB
     ( 4699717908, 'backprop/backprop 33554432'),
     ( 6442450944, 'srad/srad_v2/srad 16384 16384 0 127 0 127 0.5 2'),
     ( 7200240000, 'srad/srad_v1/srad 100 0.5 15000 15000'),
@@ -84,25 +83,49 @@ all_jobs = [
     (10456336000, 'lavaMD/lavaMD -boxes1d 110'),
     (12800320000, 'srad/srad_v1/srad 100 0.5 20000 20000'),
     (13575168000, 'lavaMD/lavaMD -boxes1d 120'),
-    #(25769803776, 'srad/srad_v2/srad 32768 32768 0 127 0 127 0.5 2'), # seems halved to 12GB
+
+
+    #
+    # Don't use:
+    #
+
+    # 50s to complete, when its memory footprint peers are around 0.5s - 15s
+    #(  419466152, 'particlefilter/particlefilter_float -x 2048 -y 2048 -z 100 -np 100'),
+
+    # 90s to complete, when its peers are around 5 - 30s
+    #( 3686640000, 'myocyte/myocyte.out 10000 1000 1'),
+
+    # seems halved to 12GB
+    #(25769803776, 'srad/srad_v2/srad 32768 32768 0 127 0 127 0.5 2'),
 ]
 small_jobs = []
 medium_jobs = []
 large_jobs = []
+one_to_four = [] # jobs with 1 - 4 GB of footprint
+over_four = []   # jobs with > 4 GB of footprint
 
 
-
-job_size_to_jobs = {
+# helper for "small", "medium", and "large" mixes
+job_mix_to_jobs = {
     'small': small_jobs,
     'medium': medium_jobs,
     'large': large_jobs,
 }
 
+# helper for "random" mixes
 job_bufs = [
     small_jobs,
     medium_jobs,
     large_jobs
 ]
+
+# helper for "50", "33", etc. mixes
+job_mix_to_denominator = {
+    '50': 2,
+    '33': 3,
+    '25': 4,
+    '16': 6
+}
 
 
 HELP_GPU = """gpu
@@ -114,26 +137,38 @@ GPU). This field must be one of "gtx0180", "k80", or "p100"."""
 HELP_NUM_JOBS = """num_jobs
 The number of jobs in the workload."""
 
-HELP_JOB_SIZE = """job_size
-The job_size represents the sizes of the jobs that are part of a workload.
+HELP_JOB_MIX = """job_mix
+The job_mix represents the mixes of the jobs that are part of a workload.
 It must be one of the following values:
   - small
   - medium
   - large
   - random
+  - 50
+  - 33
+  - 25
+  - 16
   - all
 They carry the following meanings:
   - small: 100% of the jobs are small
   - medium: 100% of the jobs are medium
   - large: 100% of the jobs are large
-  - random: ~33% of the jobs are of each size (small, medium, or large).
+  - random: ~33% of the jobs are of each small, medium, or large.
             The jobs from each group are randomly chosen.
             The order of jobs over the entire workload is random.
+  - 50: 1/2 of the jobs have a max of 1 - 4 GB memory footprint. The rest have
+        a footprint > 4 GB.
+  - 33: 1/3 of the jobs have a max of 1 - 4 GB memory footprint. The rest have
+        a footprint > 4 GB.
+  - 25: 1/4 of the jobs have a max of 1 - 4 GB memory footprint. The rest have
+        a footprint > 4 GB.
+  - 16: 1/6 of the jobs have a max of 1 - 4 GB memory footprint. The rest have
+        a footprint > 4 GB.
   - all: 100% of the jobs are in the workload. This is useful for debugging
          Here, num_jobs is still a required arg, but it's ignored; it'll just
          be equal to the size of all the jobs.
 Here, "small", "medium", "large" refer to the max memory footprint
-possible for a given job. Job sizes:
+possible for a given job:
   - small: all kernels < 10% GPU memory
   - medium: no large kernels, and at least 1 kernel with 10-50% GPU memory
   - large: at least 1 kernel with > 50% GPU memory"""
@@ -142,11 +177,11 @@ def usage_and_exit():
     print()
     print()
     print('Usage:')
-    print('    {} <gpu> <job_size> <num_jobs> [output_filename]'.format(sys.argv[0]))
+    print('    {} <gpu> <job_mix> <num_jobs> [output_filename]'.format(sys.argv[0]))
     print()
     print(HELP_GPU)
     print()
-    print(HELP_JOB_SIZE)
+    print(HELP_JOB_MIX)
     print()
     print(HELP_NUM_JOBS)
     print()
@@ -160,8 +195,9 @@ def parse_args():
     gpu = sys.argv[1]
     if gpu not in GPU_TO_MEM:
         usage_and_exit()
-    job_size = sys.argv[2]
-    if job_size not in ['small', 'medium', 'large', 'random', 'all']:
+    job_mix = sys.argv[2]
+    if job_mix not in ['small', 'medium', 'large', 'random',
+                       '50', '33', '25', '16', 'all']:
         usage_and_exit()
     num_jobs = sys.argv[3]
     if not num_jobs.isdigit():
@@ -169,7 +205,7 @@ def parse_args():
     output_filename = 'example_jobs_00.wl'
     if len(sys.argv) == 5:
         output_filename = sys.argv[4]
-    return gpu, job_size, int(num_jobs), output_filename
+    return gpu, job_mix, int(num_jobs), output_filename
 
 
 def construct_job_arrays(which_gpu):
@@ -185,16 +221,28 @@ def construct_job_arrays(which_gpu):
             medium_jobs.append(j[1])
         else:
             large_jobs.append(j[1])
+
+        if B > (1<<32): # 4 GB
+            over_four.append(j[1])
+        elif B > (1<<30):
+            one_to_four.append(j[1])
+
     print('small jobs:')
     for j in small_jobs:
         print('  {}'.format(j))
-    print('medium jobs:')
+    print('\nmedium jobs:')
     for j in medium_jobs:
         print('  {}'.format(j))
-    print('large jobs:')
+    print('\nlarge jobs:')
     for j in large_jobs:
         print('  {}'.format(j))
-    print()
+    print('\njobs with 1 - 4 GB footprint:')
+    for j in one_to_four:
+        print('  {}'.format(j))
+    print('\njobs with > 4 GB footprint:')
+    for j in over_four:
+        print('  {}'.format(j))
+    print('\n')
 
 
 
@@ -215,27 +263,57 @@ def generate_random_workload():
     return workload
 
 
+def generate_proportional_workload():
+    D = job_mix_to_denominator[job_mix]
+    workload = []
+    count = D
+    random.seed(int(time.time()))
+    for i in range(num_jobs):
+        count -= 1
+        if count == 0:
+            print('M')
+            count = D
+            job = one_to_four[random.randint(0, len(one_to_four)-1)]
+        else:
+            print('L')
+            job = over_four[random.randint(0, len(over_four)-1)]
+        # Append 
+        workload.append(job)
+    random.shuffle(workload)
+    return workload
+
+    
+
+
 def generate_workload():
     workload = []
-    jobs = job_size_to_jobs[job_size]
+    jobs = job_mix_to_jobs[job_mix]
     for i in range(num_jobs):
         job = jobs[random.randint(0, len(jobs)-1)]
         workload.append(job)
     return workload
 
 
-gpu, job_size, num_jobs, output_filename = parse_args()
+gpu, job_mix, num_jobs, output_filename = parse_args()
 construct_job_arrays(gpu)
 
-if job_size == 'random':
+print('Generating the workload for a "{}" mix...'.format(job_mix))
+
+if job_mix == 'random':
+    print('...using generate_random_workload()')
     workload = generate_random_workload()
-elif job_size == 'all':
+elif job_mix == 'all':
+    print('...using zip()')
     _, workload = zip(*all_jobs)
+elif job_mix in ['50', '33', '25', '16']:
+    print('...using generate_proporational_workload()')
+    workload = generate_proportional_workload()
 else:
+    print('...using generate_workload()')
     workload = generate_workload()
 
-
-print('Generating the workload...')
+print()
+print('Workload:')
 fp = open(output_filename, 'w')
 for j in workload:
     full_cmd = RODINIA_BMARK_PATH + '/' + j
@@ -243,5 +321,5 @@ for j in workload:
     fp.write(full_cmd+'\n')
 fp.close()
 print()
-print('Jobs written to {}'.format(output_filename))
+print('Workload also written to {}'.format(output_filename))
 print()
