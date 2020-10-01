ef1a85531905cd3d54bcad302e89c53782e83dc8
r jantz <porter@gatech.edu>
2020-10-01 23:06:58 +0000

    create gtx 1080 subfolder for coexecute slowdown

--- Benchmarks/rodinia_cuda_3.1/cuda/myocyte/gt_path_hack.h
+++ Benchmarks/rodinia_cuda_3.1/cuda/myocyte/gt_path_hack.h
@@ -3,6 +3,7 @@
 
 // XXX This will fail if/when our folder structure changes, or if we don't
 // clone into the HOME
-#define BASE_DATA_PATH "/home/cc/GPU-Sched/Benchmarks/rodinia_cuda_3.1/data/myocyte"
+//#define BASE_DATA_PATH "/home/cc/GPU-Sched/Benchmarks/rodinia_cuda_3.1/data/myocyte"
+#define BASE_DATA_PATH "/home/ubuntu/GPU-Sched/Benchmarks/rodinia_cuda_3.1/data/myocyte"
 
 #endif
