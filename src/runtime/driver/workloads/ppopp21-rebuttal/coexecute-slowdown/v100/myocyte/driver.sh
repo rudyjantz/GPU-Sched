41b39efba8bfbdb7fc1ce71cb79b52a34abae5dc
r jantz <porter@gatech.edu>
2020-10-03 13:54:43 +0000

    myocyte driver goes up to 48 procs only for v100 now

--- src/runtime/driver/workloads/ppopp21-rebuttal/coexecute-slowdown/v100/myocyte/driver.sh
+++ src/runtime/driver/workloads/ppopp21-rebuttal/coexecute-slowdown/v100/myocyte/driver.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Note to future self: never got beyond 64 jobs because it was throwing errors
-# already on sloppyjoe at that count
-#for j in 1 2 4 8 16 32 64 128 256; do
 
-for j in 1 2 4 8 16 32 64; do
+#for j in 1 2 4 8 16 32 64; do
 #for j in 128 256 512; do
+for j in 1 2 4 8 16 32 48; do # only up to 48 procs on volta mps
     echo "Running ${j}-job workload..."
     bash coexecute_slowdown_${j}jobs_1.wl &> result_${j}jobs.out
 done
